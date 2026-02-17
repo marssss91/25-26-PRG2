cd907cba36d2f8b9696c23c60c8d3dc7a1032467
midas@uneat <javier.liano@alumnos.uneatlantico.es>
2026-02-17 13:29:02 +0100

    Commit 2 — Reemplazar String[][] tableroOculto por boolean[][]

--- entregas/paredes.fernanda/src/reto-003/buscamina.java
+++ entregas/paredes.fernanda/src/reto-003/buscamina.java
@@ -10,14 +10,7 @@ public class Buscaminas {
         int columnaY = 0;
         boolean salida = false;
 
-        String[][] tableroOculto = new String[6][8];
-
-        tableroOculto[0][0] = "**"; tableroOculto[0][1] = " 1"; tableroOculto[0][2] = " 2"; tableroOculto[0][3] = " 3"; tableroOculto[0][4] = " 4"; tableroOculto[0][5] = " 5"; tableroOculto[0][6] = " 6"; tableroOculto[0][7] = " 7";
-        tableroOculto[1][0] = " 1"; tableroOculto[1][1] = "  "; tableroOculto[1][2] = "  "; tableroOculto[1][3] = "  "; tableroOculto[1][4] = "  "; tableroOculto[1][5] = "  "; tableroOculto[1][6] = "  "; tableroOculto[1][7] = "  ";
-        tableroOculto[2][0] = " 2"; tableroOculto[2][1] = "  "; tableroOculto[2][2] = "  "; tableroOculto[2][3] = "  "; tableroOculto[2][4] = "  "; tableroOculto[2][5] = "  "; tableroOculto[2][6] = "  "; tableroOculto[2][7] = "  ";
-        tableroOculto[3][0] = " 3"; tableroOculto[3][1] = "  "; tableroOculto[3][2] = "  "; tableroOculto[3][3] = "  "; tableroOculto[3][4] = "  "; tableroOculto[3][5] = "  "; tableroOculto[3][6] = "  "; tableroOculto[3][7] = "  ";
-        tableroOculto[4][0] = " 4"; tableroOculto[4][1] = "  "; tableroOculto[4][2] = "  "; tableroOculto[4][3] = "  "; tableroOculto[4][4] = "  "; tableroOculto[4][5] = "  "; tableroOculto[4][6] = "  "; tableroOculto[4][7] = "  ";
-        tableroOculto[5][0] = " 5"; tableroOculto[5][1] = "  "; tableroOculto[5][2] = "  "; tableroOculto[5][3] = "  "; tableroOculto[5][4] = "  "; tableroOculto[5][5] = "  "; tableroOculto[5][6] = "  "; tableroOculto[5][7] = "  ";
+        boolean[][] tableroOculto = new boolean[6][8];
 
         String[][] tableroVisible = new String[6][8];
 
@@ -30,43 +23,23 @@ public class Buscaminas {
 
         filaA = (int)((Math.random() * 5) + 1);
         columnaB = (int)((Math.random() * 7) + 1);
-        for (int x = 1; x < tableroOculto.length; x++) {
-            for (int y = 1; y < 8; y++) {
-                tableroOculto[filaA][columnaB] = "MM";
-            }
-        }
+        tableroOculto[filaA][columnaB] = true;
 
         filaA = (int)((Math.random() * 5) + 1);
         columnaB = (int)((Math.random() * 7) + 1);
-        for (int x = 1; x < tableroOculto.length; x++) {
-            for (int y = 1; y < 8; y++) {
-                tableroOculto[filaA][columnaB] = "MM";
-            }
-        }
+        tableroOculto[filaA][columnaB] = true;
 
         filaA = (int)((Math.random() * 5) + 1);
         columnaB = (int)((Math.random() * 7) + 1);
-        for (int x = 1; x < tableroOculto.length; x++) {
-            for (int y = 1; y < 8; y++) {
-                tableroOculto[filaA][columnaB] = "MM";
-            }
-        }
+        tableroOculto[filaA][columnaB] = true;
 
         filaA = (int)((Math.random() * 5) + 1);
         columnaB = (int)((Math.random() * 7) + 1);
-        for (int x = 1; x < tableroOculto.length; x++) {
-            for (int y = 1; y < 8; y++) {
-                tableroOculto[filaA][columnaB] = "MM";
-            }
-        }
+        tableroOculto[filaA][columnaB] = true;
 
         filaA = (int)((Math.random() * 5) + 1);
         columnaB = (int)((Math.random() * 7) + 1);
-        for (int x = 1; x < tableroOculto.length; x++) {
-            for (int y = 1; y < 8; y++) {
-                tableroOculto[filaA][columnaB] = "MM";
-            }
-        }
+        tableroOculto[filaA][columnaB] = true;
 
         while (!salida) {
             for (int x = 0; x < tableroVisible.length; x++) {
@@ -77,20 +50,19 @@ public class Buscaminas {
                 System.out.println("|");
             }
 
-            System.out.println("introduzca posicion X");
+            System.out.println("Introduzca posicion X");
             Scanner sc = new Scanner(System.in);
             filaX = sc.nextInt();
 
             System.out.println("Introduzca posicion Y");
-            Scanner sc2 = new Scanner(System.in);
             columnaY = sc.nextInt();
 
-            if ((tableroOculto[filaX][columnaY]) == "  ") {
-                tableroVisible[filaX][columnaY] = "--";
-                celdasVacias++;
-            } else {
+            if (tableroOculto[filaX][columnaY]) {
                 tableroVisible[filaX][columnaY] = "**";
                 minas++;
+            } else {
+                tableroVisible[filaX][columnaY] = "--";
+                celdasVacias++;
             }
 
             for (int x = 0; x < tableroVisible.length; x++) {
